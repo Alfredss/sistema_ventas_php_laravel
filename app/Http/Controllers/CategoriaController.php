068334b2d0bd9b34174eb43fcd92877979d430b8
Alfredss <alfredss.24.7@gmail.com>
2016-11-07 19:11:33 -0500

    Creamos los metodos index(), create(), store(), show(), edit(), update(), destroy() en CategoriaController

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -12,14 +12,28 @@ use DB;
 
 class CategoriaController extends Controller {
 
+	public function __construct()
+	{
+
+	}
 	/**
 	 * Display a listing of the resource.
 	 *
 	 * @return Response
 	 */
-	public function index()
+	public function index(Request $request)
 	{
-		//
+		//obtenemos los registros de la tabla categoria
+		if($request)
+		{
+			$query = trim($request -> get('searchText'));//almacena el texto de busqueda
+			$categorias = DB::table('categoria') -> where('nombre','LIKE','%'.$query.'%')
+			->where('condicion', '=', '1')
+			->orderBy('idcategoria', 'desc')
+			->paginate(7);//condicion de la busqueda en la tabla categoria
+
+			return view('almacen.categoria.index', ["categorias" => $categorias, "searchText"=>$query]);
+		} // si el objeto existe
 	}
 
 	/**
@@ -30,6 +44,7 @@ class CategoriaController extends Controller {
 	public function create()
 	{
 		//
+		return view("almacen.categoria,create");
 	}
 
 	/**
@@ -37,9 +52,15 @@ class CategoriaController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function store()
+	public function store(CategoriaFormRequest $request)
 	{
-		//
+		//para almacenar nuestro objeto categoria en la tabla categoria de la base de datos
+		$categoria = new Categoria;
+		$categoria->nombre=$request->get('nombre');
+		$categoria->descirpcion=$request->get('descripcion');
+		$categoria->condicion='1';
+		$categoria->save();
+		return Redirect::to('almacen/categoria');
 	}
 
 	/**
@@ -50,7 +71,8 @@ class CategoriaController extends Controller {
 	 */
 	public function show($id)
 	{
-		//
+		//para mostrar una vista
+		return view("almacen.categoria.show",["categoria"=>Categoria::findOrFail($id)]);//llamame a la vista y enviale esta categoria para que la muestre
 	}
 
 	/**
@@ -62,6 +84,7 @@ class CategoriaController extends Controller {
 	public function edit($id)
 	{
 		//
+		return view("almacen.categoria.edit",["categoria"=>Categoria::findOrFail($id)]);//llamame a la vista y enviale esta categoria para que la modificarlos
 	}
 
 	/**
@@ -70,9 +93,15 @@ class CategoriaController extends Controller {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function update($id)
+	public function update(CategoriaFormRequest $request,$id)
 	{
-		//
+		//almacenamos o que hacemos en edit
+		$categoria = Categoria::findOrFail($id);
+		$categoria->nombre=$request->get('nombre');
+		$categoria->descripcion=$request->get('descripcion');
+		$categoria->update();
+
+		return Redirect::to('almacen/categoria');
 	}
 
 	/**
@@ -83,7 +112,12 @@ class CategoriaController extends Controller {
 	 */
 	public function destroy($id)
 	{
-		//
+		//ponemos la condicion de la categoria a 0
+		$categoria = Categoria::findOrFail($id);
+		$categoria->condicion='0';
+		$categoria->update();
+		return Redirect::to('almacen/categoria');
+
 	}
 
 }
